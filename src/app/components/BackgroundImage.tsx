bb13b166c4b78193666bc6556430b54379ee0203
Javacadu <jacobatanacio@gmail.com>
2025-07-31 09:29:42 -0700

    quick fix to the backgtound image component because it wasnt displaying image for some reason

--- src/app/components/BackgroundImage.tsx
+++ src/app/components/BackgroundImage.tsx
@@ -11,7 +11,12 @@ export default function BackgroundImage({image,title}: pageProps) {
 
     return (
         <div>
-            <div className={"header-image fade-in bg-[linear-gradient(180deg,rgba(15,108,182,0)26%,rgba(0,89,160,0.722)100%),url(" + image + ")]"}/>
+            <div
+                className="header-image"
+                style={{
+                    backgroundImage: `linear-gradient(180deg,rgba(15,108,182,0) 26%,rgba(0,89,160,0.722) 100%), url(${image})`
+                }}
+            ></div>
 
             <div className="w-full h-10 absolute z-3 flex gap-11">
             <h1 className="z-6 w-fit my-auto mr-auto leading-tight flex flex-col">
