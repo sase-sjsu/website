2714d02dbef670ca1080f64ce265b0a8eeb51b46
Amy Okuma <amyokuma@gmail.com>
2025-08-12 23:25:05 -0700

    add description section and calendar

--- src/app/components/BackgroundImage.tsx
+++ src/app/components/BackgroundImage.tsx
@@ -1,28 +1,27 @@
 //component for static background image used for all other pages EXCEPT home
 
-"use client"
+"use client";
 
 type pageProps = {
-    image: string;
-    title: string;
-}
+  image: string;
+  title: string;
+};
 
-export default function BackgroundImage({image,title}: pageProps) {
+export default function BackgroundImage({ image, title }: pageProps) {
+  return (
+    <div>
+      <div
+        className="header-image"
+        style={{
+          backgroundImage: `linear-gradient(180deg,rgba(15,108,182,0) 26%,rgba(0,89,160,0.722) 100%), url(${image})`,
+        }}
+      ></div>
 
-    return (
-        <div>
-            <div
-                className="header-image"
-                style={{
-                    backgroundImage: `linear-gradient(180deg,rgba(15,108,182,0) 26%,rgba(0,89,160,0.722) 100%), url(${image})`
-                }}
-            ></div>
-
-            <div className="w-full h-10 absolute z-3 flex gap-11">
-            <h1 className="z-6 w-fit my-auto mr-auto leading-tight flex flex-col">
-                {title}
-            </h1>
-            </div>
-        </div>
-    )
+      <div className="w-full h-10 absolute z-3 flex gap-11">
+        <h1 className="z-6 w-fit my-auto mr-auto leading-tight flex flex-col">
+          {title}
+        </h1>
+      </div>
+    </div>
+  );
 }
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,7 +2,65 @@ import BackgroundImage from "../components/BackgroundImage";
 export default function EventsPage() {
   return (
     <>
-      <BackgroundImage image="/events/events_header_img.jpg" title="Events" />
+      <header>
+        <BackgroundImage image="/events/events_header_img.jpg" title="Events" />
+      </header>
+      <section className="pt-20 px-96 text-center">
+        <p>
+          Throughout the school year, SJSU SASE hosts a variety of events. These
+          events may be professional, social, or even a mix of both! Follow our
+          Instagram and join our Discord server to get updates on events and
+          other club activities right away!
+        </p>
+      </section>
+      <section>
+        <h2>Upcoming Events</h2>
+        <div className="pt-16 max-w-2xl mx-auto text-left">
+          <ol className="list-decimal list-inside font-bold text-xl">
+            <li>
+              <span className="font-bold">Date:</span>{" "}
+              <span className="font-normal">
+                Event Name loream loreamloreamloreamloreamloreamloream
+              </span>
+            </li>
+            <li>
+              <span className="font-bold">Date:</span>{" "}
+              <span className="font-normal">
+                Event Name loream loreamloreamloreamloreamloreamloream
+              </span>
+            </li>
+            <li>
+              <span className="font-bold">Date:</span>{" "}
+              <span className="font-normal">
+                Event Name loream loreamloreamloreamloreamloreamloream
+              </span>
+            </li>
+            <li>
+              <span className="font-bold">Date:</span>{" "}
+              <span className="font-normal">
+                Event Name loream loreamloreamloreamloreamloreamloream
+              </span>
+            </li>
+          </ol>
+        </div>
+      </section>
+      <section>
+        <h2>Past Events</h2>
+      </section>
+      <section className="px-20">
+        <iframe
+          src="https://embed.styledcalendar.com/#dH12e7pjpSoQTADB9h69"
+          title="Styled Calendar"
+          className="w-full h-[42rem] border-0"
+          data-cy="calendar-embed-iframe"
+        ></iframe>
+
+        <script
+          async
+          type="module"
+          src="https://embed.styledcalendar.com/assets/parent-window.js"
+        ></script>
+      </section>
     </>
   );
 }
