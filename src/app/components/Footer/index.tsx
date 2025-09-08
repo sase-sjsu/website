60b3b34e5b8fc3db8ee90582a3aabad9311cc214
ashley-rr <160376224+ashley-rr@users.noreply.github.com>
2025-09-07 20:42:36 -0700

    feature: added media queries for footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,20 +1,24 @@
+import styles from "./Footer.module.css"
+
 export default function Footer() {
   return (
-    <footer className="flex border-t-1 mt-100 text-gray-400 border-gray-400 px-5 h-17 items-center justify-center">
-      <div className="mr-auto w-50 ">
-        <img className="w-auto mb-1.5 h-6" src="/footer/footer_sase_logo.png"></img>
-        <img className="w-auto h-5" src="/footer/footer_sjsu_logo.png"></img>
+    <footer className={styles.footerContainer}>
+    
+      <div className={styles.logoContainer}>
+        <img src="/footer/footer_sase_logo.png"></img>
+        <img src="/footer/footer_sjsu_logo.png"></img>
       </div>
 
-      <p style={{fontSize: "medium"}} className="text-center w-fit">
-        San Jose State University​, 1 Washington Sq, San Jose, CA 
-        <br/>SASE at SJSU © 2025
-      </p>
+      <div className={styles.address}>
+        <p>San Jose State University​, 1 Washington Sq, San Jose, CA </p>
+        <p>SASE at SJSU © 2025</p>
+      </div>
 
-      <p style={{fontSize: "medium"}} className="text-right ml-auto w-50">
+      <div  className={styles.creator}>
         made with 💙💚
-        <br/> SASE SJSU webdev team
-      </p>
+        <p>SASE SJSU webdev team</p>
+      </div>
+
     </footer>
   )
 }
