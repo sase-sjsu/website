38998a521bdce1973b3fd3e9f9ed95354f8678ab
ashley-rr <160376224+ashley-rr@users.noreply.github.com>
2026-01-30 11:18:24 -0800

    comment out calendar section

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -5,14 +5,34 @@ export default function EventsPage() {
       <header>
         <BackgroundImage image="/events/events_header_img.jpg" title="Events" />
       </header>
-      <section className="pt-20 px-96 text-center">
-        <p>
+
+      <section className="w-4xl mx-auto text-center">
+        <p className="my-20">
           Throughout the school year, SJSU SASE hosts a variety of events. These
           events may be professional, social, or even a mix of both! Follow our
           Instagram and join our Discord server to get updates on events and
           other club activities right away!
         </p>
       </section>
+{/* 
+      <section className="flex flex-col items-center">
+        <div className="text-left w-4xl mx-auto  p-10 bg-opacity-50 rounded-lg">
+          <p className="text-2xl mb-4">
+          Welcome to SJSU’s Society of Asian Scientists and Engineers (SASE)!<br></br><br></br>
+          We are a fully free to join organization open to all students on campus, regardless of background, offering a wide range of professional development and social opportunities to help you grow, connect, and develop a sense of community on campus!
+          </p>
+        
+          <div className="flex flex-row gap-10 items-center h-1">
+            <img className="z-6 h-45 mt-45" src="/about_us/eboard-alan.jpg"/>
+              <div className="flex flex-col gap-2 mt-45">
+                <p><b>Alan To</b></p>
+                <p>SJSU SASE President</p>
+                <p>25-26</p>
+              </div>
+          </div>
+        </div>
+      </section> */}
+
       <section>
         <h2>Upcoming Events</h2>
         <div className="pt-16 max-w-2xl mx-auto text-left">
